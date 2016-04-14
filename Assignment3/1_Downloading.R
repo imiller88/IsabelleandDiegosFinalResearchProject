7ca2721ad152485c5b377567ed3247d1b31430d9
Diego Fernández Fernández <dgdoblecero@gmail.com>
2016-04-14 17:17:52 +0200

    Subsetting dataset 1

--- Assignment3/1_Downloading.R
+++ Assignment3/1_Downloading.R
@@ -1,14 +1,14 @@
 getwd
 setwd("/Users/Diegotab/GitHub/IsabelleandDiegosFinalResearchProject/Assignment3")
-#1. Gathering Quality of Government Standard Dataset 
+#1. Gathering data from Quality of Government Standard Dataset 
 URL <- 'http://www.qogdata.pol.gu.se/data/qog_std_ts_jan16.csv'
 temp <- tempfile()
 download.file(URL, temp)
 QoGData <- read.csv(gzfile(temp, "qog_std_ts_jan16.csv"))
 unlink (temp)
 
-mywars <- c("cname", "year")
-QoGDatareduced <- QoGData[mywars]
+#2. Subsetting (selection of years and variables)
+QoGDatareduced <- subset(QoGData, year>2001, select=c("cname", "year", "fh_aor", "fh_fotpsc", "al_ethnic", "wdi_gini", "al_language", "wef_ji", "wdi_gdpcur"))
 
 #2.Gathering data from World Governance Indicators
 URL2 <- ""
