3326a95e0520bb744544dfa5483f97f2f9878bf9
Diego Fernández Fernández <dgdoblecero@gmail.com>
2016-04-14 19:14:55 +0200

    Dataset 3

--- Assignment3/1_Downloading.R
+++ Assignment3/1_Downloading.R
@@ -8,14 +8,16 @@ QoGData <- read.csv(gzfile(temp, "qog_std_ts_jan16.csv"))
 unlink (temp)
 
 #2. Subsetting (selection of years and variables)
-QoGDatareduced <- subset(QoGData, year>2001, select=c("cname", "year", "fh_aor", "fh_fotpsc", "al_ethnic", "wdi_gini", "al_language", "wef_ji", "wdi_gdpcur"))
+QoGDatareduced <- subset(QoGData, year>2001, select=c("cname", "year", "fh_aor", "fh_fotpsc", "al_ethnic", "al_language", "wef_ji"))
 
-#2.Gathering data from World Governance Indicators
+#2.Gathering WGI data
 download.file("http://databank.worldbank.org/data/download/WGI_csv.zip",
               temp, mode="wb")
 unzip(temp, "WGI_Data.csv")
 wgi_allyears <- read.table("WGI_Data.csv", 
                  sep=",",skip=2, header=T)
 
-
-
+#3.Gathering WDI data
+library (WDI)
+Gini <- WDI(indicator="SI.POV.GINI")
+GDPcapita <- WDI(indicator="NY.GDP.PCAP.CD")
