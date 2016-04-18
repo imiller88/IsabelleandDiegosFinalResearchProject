af41deffd36be236fa1b5c5c141a9c0728c437c1
ISABELLE <isabelle.k.miller@gmail.com>
2016-04-18 09:41:32 +0200

    some stuff

--- Assignment3/DescriptiveStats.R
+++ Assignment3/DescriptiveStats.R
@@ -1,16 +1,21 @@
-names(CombinedVars)
-#reorder to put dependent variables first
-CombinedVars <- CombinedVars[c("iso2c", "cname", "year", "pressfreedom", "orgassfreedom", "ethnicfrac",
-    "Ginicoef", "GDPpercapita", "VoiceandAccountability", "langfrac", "judindep")] 
-#check NAs & summary statistics
-summary(CombinedVars)
-
+#declare panel data
 library(plm)
-#setting data as panel data
 CombinedVars <- plm.data(CombinedVars, index=c("cname", "year"))
 #did the NAs stay?
 
+#view dataset
+names(CombinedVars) #list variables
+str(CombinedVars) #overview of dataset structure
 
+#reorder to put dependent variables first
+CombinedVars <- CombinedVars[c("iso2c", "cname", "year", "pressfreedom", "orgassfreedom", "ethnicfrac",
+    "Ginicoef", "GDPpercapita", "VoiceandAccountability", "langfrac", "judindep")] 
+
+#check NAs for each variable
+colSums(is.na(CombinedVars)) 
+CombinedVars2 <- na.omit(CombinedVars) #create dataset that omits NAs
+
+library(dygraphs)
 
 #figure out how to do if year
 
