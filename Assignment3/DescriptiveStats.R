4a079da76d32f1edfc719162a69caff3ef5a5b03
ISABELLE <isabelle.k.miller@gmail.com>
2016-04-18 17:50:44 +0200

    some stuff

--- Assignment3/DescriptiveStats.R
+++ Assignment3/DescriptiveStats.R
@@ -1,45 +1,48 @@
-#declare panel data
-library(plm)
-CombinedVars2 <- plm.data(CombinedVars2, index=c("cname", "year"))
-#did the NAs stay?
+library("ggmap")
+library("maptools")
+library("ggplot2")
+library("rworldmap")
+library("sp")
+library('rworldmap')
+library("dygraphs")
+
+
 
 #view dataset
 names(CombinedVars2) #list variables
 str(CombinedVars2) #overview of dataset structure
+table(CombinedVars2$year)
 
-
-library(dygraphs)
-library(networkD3)
-
-hist(CombinedVars2$pressfreedom, year==2005,
-     main = "Press Freedom Indicator",
-     ylab = "Press Freedom Score",
-     xlab = "Number of Countries")
 
 #boxplot by year
-plot(pressfreedom ~ year, data = CombinedVars2, 
+boxplot(pressfreedom ~ year, data = CombinedVars2, 
      xlab = "Year", las = 1,
      ylab = "Freedom Score",
      main = "Press Freedom Indicator")
 
-plot(orgassfreedom ~ year, data = CombinedVars2, 
+boxplot(orgassfreedom ~ year, data = CombinedVars2, 
      xlab = "Year", las = 1,
      ylab = "Freedom Score",
      main = "Freedom of Organization and Association")
 
+boxplot(CombinedVars2$ethnicfrac,
+        ylab = "Degree of Fractionalization",
+        main = "Distribution of Degrees of Ethnic Fractionalization")
+
+#histograms of press freedom over time
 CV2002 <- subset(CombinedVars2, year == 2002)
 hist(CV2002$pressfreedom,
-     breaks=10,
-     xlim=c(100,1))
+     breaks=10, #puts countries into bins
+     xlim=c(100,1)) #reverse x axis
 
 CV2005 <- subset(CombinedVars2, year == 2005)
 hist(CV2005$pressfreedom, 
-     breaks=10, #equal numbers of countries in each range except top range
-     xlim=c(100,1)) #reverse x axis
+     breaks=10, 
+     xlim=c(100,1)) 
 
 CV2010 <- subset(CombinedVars2, year == 2010)
 hist(CV2010$pressfreedom, 
-     breaks=10, #more countries move down
+     breaks=10, 
      xlim=c(100,1)) 
 
 CV2014 <- subset(CombinedVars2, year == 2014)
@@ -48,5 +51,48 @@ hist(CV2014$pressfreedom,
      xlim=c(100,1))
 
 hist(CV2005$orgassfreedom)
+
+#rworldmap maps
+#2002
+sPDF <- joinCountryData2Map( CV2002
+                             ,joinCode = "ISO2"
+                             ,nameJoinColumn = "iso2c")
+
+#pressfreedom 2002
+mapCountryData(sPDF, nameColumnToPlot='pressfreedom', mapTitle= 'Press Freedom 2002',
+               colourPalette = c("heat"),
+               catMethod = 'fixedWidth',
+               numCats = 8,
+               borderCol='black')
+
+#ethnicfrac 2002 (higher = more fractionalized)
+mapCountryData(sPDF, nameColumnToPlot='ethnicfrac', mapTitle= 'Ethnic Fractionalization (2002)',
+               colourPalette = c("magenta", "blue", "darkorange", "green"),
+               catMethod = 'fixedWidth',
+               numCats = 4,
+               borderCol='black')
+
+#2014
+sPDF <- joinCountryData2Map( CV2014
+                             ,joinCode = "ISO2"
+                             ,nameJoinColumn = "iso2c")
+
+#pressfreedom 2014
+mapCountryData(sPDF, nameColumnToPlot='pressfreedom', mapTitle= 'Press Freedom 2014',
+               colourPalette = c("heat"),
+               catMethod = 'fixedWidth',
+               numCats = 8,
+               borderCol='black')
+
+#ethnicfrac 2014
+mapCountryData(sPDF, nameColumnToPlot='ethnicfrac', mapTitle= 'Ethnic Fractionalization (2014)',
+               colourPalette = c("magenta", "blue", "darkorange", "green"),
+               catMethod = 'fixedWidth',
+               numCats = 4,
+               borderCol='black')
+
+
+
+
 
 
